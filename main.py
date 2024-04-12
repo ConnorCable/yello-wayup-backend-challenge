e96aa5c6d23628028b69cdd9997f462850eebe4a
ConnorCable <ccab0528@gmail.com>
2024-04-11 21:12:24 -0700

    first implementation of encode and decode functions

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, jsonify
+import base64
 
 app = Flask(__name__)
 
 
 @app.route("/encode")
 def encode():
-    pass
+    data = request.data
+    encoded = base64.b64encode(data)
+    return jsonify({"url": "https://short.est/" + str(encoded)})
 
 
 @app.route("/decode")
 def decode():
-    pass
-
+    data = request.data
+    url = str(data).split("/")[-1]
+    decoded = base64.b64decode(url)
+    return jsonify({"url": decoded})
 
 
 if __name__ == "__main__":
